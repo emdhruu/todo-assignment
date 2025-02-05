e7d7afafbfa0d0a897466d9c3e920391a4832e0d
Dhrutii <thakkardhruti22@gmail.com>
2025-02-05 18:13:12 +0530

    initialize container and given heading

---
+++ src/components/TodoContainer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TodoContainer = () => {
+  return (
+    <div className="lg:absolute lg:top-4 mt-4 lg:mt-0 md:mt-0 md:absolute md:top-4 w-full flex justify-center">
+      <h1 className="lg:text-5xl text-4xl font-extralight dark:text-white">
+        Welcome Back, buddy 👋
+      </h1>
+    </div>
+  );
+};
+
+export default TodoContainer;
