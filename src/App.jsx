639fca8cfbcdae31ff8c2568dfba676c6bca7d8b
Dhrutii <thakkardhruti22@gmail.com>
2025-02-05 18:10:48 +0530

    Add toogle btn and todo container

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import useThemeStore from "./store/themeStore";
+import TodoContainer from "./components/TodoContainer";
+// import ToggleButton from "./components/ToggleButton";
 
 const App = () => {
-  return (
-    <div className='text-red-500'>App</div>
-  )
-}
+  const { darkMode, toggleTheme } = useThemeStore();
 
-export default App
+  // Apply theme on initial load
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [darkMode]);
+
+  return (
+    <div className="relative h-screen flex flex-col items-center justify-center transition-all bg-white dark:bg-black">
+      {/* Background with Gradient and Mask */}
+      <div
+        className={`absolute inset-0 w-full h-full bg-[radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)] dark:bg-[radial-gradient(125%_125%_at_50%_10%,#000_40%,#9333ea_100%)]`}
+      />
+      <div className="relative w-full h-full z-10">
+        <div className="relative flex flex-col items-end mr-3 md:mb z-20">
+          <button
+            onClick={toggleTheme}
+            className="mt-4 px-4 py-2 rounded bg-blue-500 text-white dark:bg-yellow-500"
+          >
+            Toggle Theme
+          </button>
+        </div>
+        {/* Todo Container */}
+        <div className="w-full z-10">
+          <TodoContainer />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
