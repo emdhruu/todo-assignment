69cd01bd8a3ff4ab3302c383140f3680f740fd69
Dhrutii <thakkardhruti22@gmail.com>
2025-02-06 07:37:07 +0530

    add(ui): Heading to it

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <h1 className="lg:text-5xl text-3xl font-thin dark:text-gray-300 text-black w-full">
+      Welcome Back, buddy 👋
+    </h1>
+  );
+};
+
+export default Header;
