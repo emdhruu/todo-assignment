4760f1b6d98b3cd587592ff4462fda7b3f7e4184
Dhrutii <thakkardhruti22@gmail.com>
2025-02-06 17:44:59 +0530

    add: header and form component

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Header from "./Header.jsx";
+import TodoForm from "./TodoForm.jsx";
 
 const TodoContainer = () => {
   return (
-    <div className="lg:absolute lg:top-4 mt-4 lg:mt-0 md:mt-0 md:absolute md:top-4 w-full flex justify-center">
-      <h1 className="lg:text-5xl text-4xl font-extralight dark:text-white">
-        Welcome Back, buddy 👋
-      </h1>
+    <div className="min-w-full lg:max-w-4xl md:max-w-3xl lg:px-[20%] md:px-[10%] px-2 lg:absolute lg:top-4 mt-4 lg:mt-0 md:mt-0 md:absolute md:top-4 w-full flex flex-col justify-center items-center ">
+      <Header />
+      <TodoForm />
     </div>
   );
 };
